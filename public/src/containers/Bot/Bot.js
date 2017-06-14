0abdc2f5c414fba401fec9afeed1a13a850969a6
Vladimir <vlad@simplytechnologies.net>
2017-06-14 15:39:28 +0400

    some changes

--- public/src/containers/Bot/Bot.js
+++ public/src/containers/Bot/Bot.js
@@ -24,7 +24,6 @@ export class Bot extends Component {
             typing: false,
             catId: null
         };
-
     }
 
     componentWillMount() {
@@ -138,7 +137,6 @@ export class Bot extends Component {
                 )
             }
             else if (message.type == 'details') {
-                console.log(message.list)
                 let self = this;
                 return (
                     <div key={id} className={"row " + styles.catContainer}>
@@ -169,7 +167,7 @@ export class Bot extends Component {
     scrollBottom = () => {
         if ($('.msg_block').length > 0) {
             $('html, body').animate({
-                scrollTop: $('.msg_block').last().offset().top + 0
+                scrollTop: $('.msg_block').last().offset().top + 50
             }, 1);
         }
 
@@ -196,40 +194,38 @@ export class Bot extends Component {
     };
 
     greetingMessage = () => {
+        let greetingList = Data.greeting;
 
-        let self = this;
-
-        return Data.greeting.map(response => {
-            self.addMessage(response.text, self.MSG_RECEIVE_TYPE, response.type);
-
-        })
+        for (let i = 0; i < greetingList.length; i++) {
+            this.addMessage(greetingList[i].text, this.MSG_RECEIVE_TYPE, greetingList[i].type);
+        }
     };
 
     getStarted = () => {
-        let self = this;
+        let catergoryList = Data.category;
 
-        return Data.category.map(response => {
-
-            if (response.type === 'category') {
-                self.addContent(response.items, response.type)
-
+        for (let i = 0; i < catergoryList.length; i++) {
+            if(catergoryList[i].type == 'category') {
+                this.addContent(catergoryList[i].items, catergoryList[i].type)
             } else {
-                self.addMessage(response.text, self.MSG_RECEIVE_TYPE, response.type);
+                this.addMessage(catergoryList[i].text, this.MSG_RECEIVE_TYPE, catergoryList[i].type);
+
             }
-        });
+        }
     };
 
     openDetails = (catId, title) => {
         this.addMessage(title, this.MSG_SENT_TYPE, 'text');
-        let catDetails = Data.categoryDetails[catId];
-        let self = this;
-        return catDetails.map(response => {
-            if (response.type === 'details') {
-                self.addContent(response.items, response.type)
+
+        let catDetailsList = Data.categoryDetails[catId];
+
+        for (let i = 0; i < catDetailsList.length; i++) {
+            if (catDetailsList[i].type === 'details') {
+                this.addContent(catDetailsList[i].items, catDetailsList[i].type)
             } else {
-                self.addMessage(response.text, self.MSG_RECEIVE_TYPE, response.type);
+                this.addMessage(catDetailsList[i].text, this.MSG_RECEIVE_TYPE, catDetailsList[i].type);
             }
-        })
+        }
     };
 
 
